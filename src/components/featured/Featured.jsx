bd32f82e490dab4c033b0d348813df8c10bcc4de
KneerazPandey <nirajpandeyniraj77@gmail.com>
2023-07-19 17:54:20 +0545

    Added total revenue circular progress and revenue area chart including customer tables

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { 
+  BiDotsVerticalRounded, 
+  BiSolidArrowFromBottom, 
+  BiSolidArrowFromTop} from 'react-icons/bi';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import './featured.scss';
 import 'react-circular-progressbar/dist/styles.css';
@@ -22,6 +25,29 @@ const Featured = () => {
         <p className="title">Total sales made today</p>
         <p className="amount">$420</p>
         <p className="desc">Previous transactions processing. Last payments may not be included.</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult positive">
+              <BiSolidArrowFromBottom  fontSize={'small'} />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult negative">
+              <BiSolidArrowFromTop fontSize={'small'} />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <BiSolidArrowFromBottom fontSize={'small'} />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
